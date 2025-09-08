be5ff57338a849432f0f4690881a3f1f4f7cd6ea
estiaqqq <estiaqq.ahmed@gmail.com>
2025-09-08 23:39:38 +0600

    cave pattern fix

--- cave_pattern.py
+++ cave_pattern.py
@@ -9,9 +9,14 @@ row = ""
 col = ""
 i = 1
 j = 1
-user_input_number = 5
+user_input_number = 4
 
-while (i < (user_input_number*2 + 1)):
+max_row = user_input_number * \
+    2 - 1 if user_input_number % 2 == 0 else user_input_number * 2 - 1
+
+print("max row - ", max_row)
+
+while (i <= max_row):
     if (i < user_input_number):
         while (j <= (user_input_number - i + 1)):
             row = row + "* "
@@ -24,7 +29,7 @@ while (i < (user_input_number*2 + 1)):
         print("*")
     elif (i > user_input_number):
 
-        while (j <= i - user_input_number):
+        while (j <= i - user_input_number + 1):
             row = row + "* "
             j += 1
         print(row)
